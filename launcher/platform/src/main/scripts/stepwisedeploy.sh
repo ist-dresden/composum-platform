4ea7bebe6c5787eabbfeca344bd40226af82b152
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-02-05 11:33:05 +0100

    rewrite stepwise deploy script to allow replacements and mixture of levels between fileinstall-user and fileinstall-docker

--- launcher/platform/src/main/scripts/stepwisedeploy.sh
+++ launcher/platform/src/main/scripts/stepwisedeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set -evx
+
 function logdate {
     date -u '+%d.%m.%Y %H:%M:%S'
 }
@@ -11,35 +13,48 @@ function waituntilquiet {
     `dirname $0`/WaitForServerUp.jsh $logfile
 }
 
+# First copy stuff together into intermediatedir to be able to
+# override stuff from the docker image.
+intermediatedir=/opt/sling/fileinstall-joined
+targetdir=/opt/sling/fileinstall
 
-sleep 20 # give server some undisturbed startup time
-
-echo `logdate` start stepwise deploying stuff
+/bin/rm -fr $intermediatedir $targetdir
+mkdir $intermediatedir $targetdir
 
 cd /opt/sling/fileinstall-docker
 if [ "$(ls -A .)" ]; then
     for dir in */; do
-        mkdir -p ../fileinstall/$dir
-        echo `logdate` checking `pwd`/$dir
+        mkdir -p $intermediatedir/$dir
         for fil in $dir/*; do
-            if test \! -e "../fileinstall/$fil"; then
-                echo `logdate` deploying $fil
-                ln -s `pwd`/$fil ../fileinstall/$dir/
-            fi
+            ln -s `pwd`/$fil $intermediatedir/$dir/
         done
-        waituntilquiet
     done
 fi
 
 cd /opt/sling/fileinstall-user
 if [ "$(ls -A .)" ]; then
     for dir in */; do
-        mkdir -p ../fileinstall/$dir
+        mkdir -p $intermediatedir/$dir
+        for fil in $dir/*; do
+            ln -fs `pwd`/$fil $intermediatedir/$dir/
+        done
+    done
+fi
+
+sleep 20 # give server some undisturbed startup time
+
+echo `logdate` start stepwise deploying stuff
+
+# Now stepwise move the links to the install directory
+cd $intermediatedir
+if [ "$(ls -A .)" ]; then
+    for dir in */; do
+        mkdir -p $targetdir/$dir
         echo `logdate` checking `pwd`/$dir
         for fil in $dir/*; do
-            if test \! -e "../fileinstall/$fil"; then
+            if test \! -e "$targetdir/$fil"; then
                 echo `logdate` deploying $fil
-                ln -s `pwd`/$fil ../fileinstall/$dir/
+                mv -f `pwd`/$fil $targetdir/$dir/
             fi
         done
         waituntilquiet
@@ -47,3 +62,5 @@ if [ "$(ls -A .)" ]; then
 fi
 
 echo `logdate` finished stepwise deploying stuff at `date`
+
+/bin/rm -fr $intermediatedir
