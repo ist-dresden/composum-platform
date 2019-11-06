9728c4b77303165505210807f48d48ac7b38b44b
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-11-06 16:21:14 +0100

    introduce LockAsAutoCloseable for easy use of locks in try-with-resource statement

---
+++ commons/bundle/src/main/java/com/composum/platform/commons/util/LockAsAutoCloseable.java
@@ -0,0 +1,36 @@
+package com.composum.platform.commons.util;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Allows using a {@link java.util.concurrent.locks.Lock} with a try with resources, e.g.:
+ * <code> try (LockAsAutoCloseable locked = LockAsAutoCloseable.lock(lock)) { ... } </code> .
+ */
+@SuppressWarnings("LockAcquiredButNotSafelyReleased")
+public class LockAsAutoCloseable implements AutoCloseable {
+
+    protected final Lock lock;
+
+    protected LockAsAutoCloseable(Lock lock) {
+        this.lock = lock;
+        lock.lock();
+        try (LockAsAutoCloseable locked = LockAsAutoCloseable.lock(lock)) {
+
+        }
+    }
+
+    /**
+     * Acquires a lock on the given lock and allows using it the try-with-resources way. For example:
+     * <code> try (LockAsAutoCloseable locked = LockAsAutoCloseable.lock(lock)) { ... } </code> .
+     * Always use in try-with-resource statement.
+     */
+    public static LockAsAutoCloseable lock(Lock lock) {
+        return new LockAsAutoCloseable(lock);
+    }
+
+    /** Unlocks the lock. */
+    @Override
+    public void close() {
+        lock.unlock();
+    }
+}
